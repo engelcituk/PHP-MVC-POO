4a0c03124d7d4abf6bd114b2ecc1bff802836276
citkcaamal <programaciondyn3.mx@sandos.com>
2020-03-06 17:50:48 -0500

    fix bub de errores que se presenta, en el mapeo de parametros cuando no se recibe nada en url

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -5,7 +5,8 @@
  * Crea URL y cargar del controllador nucleo
  * Formateo de la URL /controllador/metodo/parametros
  */
- class Core {
+
+class Core {
      protected $controladorActual = 'Paginas';
      protected $metodoActual = 'index';
      protected $parametros = [];
@@ -13,7 +14,7 @@
      public function __construct(){
          $url = $this->getUrl();         
          // busca en los controladores el primer valor de nuestro array url
-         if(file_exists('../app/controllers/'. ucwords($url[0]).'.php')){
+         if($url && file_exists('../app/controllers/'. ucwords($url[0]).'.php')){
              // sí existe, establezco ese controlador en la propiedad $controladorActual
              $this->controladorActual = ucwords($url[0]);
              unset($url[0]); // elimino la variable con el indice 1
@@ -47,8 +48,9 @@
              $url = filter_var($url, FILTER_SANITIZE_URL);// sanitizo la url
              $url = explode('/', $url); // divide la cadena en partes (creando un array)
              return $url; // regreso url en un array
-         }else {
-            return $this->controladorActual; // regreso lo establecido al principio
+         }else  {
+             $url[0]='Paginas';
+            return $url;
          }
      }
  }
