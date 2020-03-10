33e54596f1eb5fe965984c0d6368fbf1a83a709a
cituk <programaciondyn3.mx@sandos.com>
2020-03-09 23:37:42 -0500

    Controlador de Posts

---
+++ app/views/posts/index.php
@@ -0,0 +1,13 @@
+<?php require APPROOT . '/views/shared/header.php'; ?>
+<div class="row">
+  <div class="col-md-6">
+    <h1>Posts</h1>
+  </div>
+  <div class="col-md-6">
+        <a class="btn btn-primary pull-right" href="<?php echo URLROOT . '/posts/add'; ?>" role="button">
+        <i class="fas fa-pencil-alt"></i> Crear publicación
+        </a>
+  </div>
+</div>
+<?php require APPROOT . '/views/shared/footer.php'; ?>
+
