6cba30226a31ea35dad21fc5e6bf30a965adbd95
cituk <programaciondyn3.mx@sandos.com>
2020-03-15 00:56:41 -0500

    Activación de links y mostrar nombre de usuario conectado

--- app/helpers/urlHelper.php
+++ app/helpers/urlHelper.php
@@ -3,3 +3,29 @@
 function redirect($pagina){
     header('location: '.URLROOT.'/'.$pagina);
 }
+
+
+function activeMenu($url){
+
+    $directoryURI = $_SERVER['REQUEST_URI'];
+    $path = parse_url($directoryURI, PHP_URL_PATH);
+    $components = explode('/', $path);
+
+    $first_part = $components[1];
+    $second_part = $components[2];
+    $third_part = $components[3];
+
+    if($third_part && $third_part == $url){
+        return $active = 'active';
+    }
+    if($second_part  && $second_part == $url){
+        return $active = 'active';
+    }
+
+    if($first_part  && $first_part == $url && !$second_part && !$third_part){
+        return $active = 'active';
+    }
+   
+    return $active;
+}
+
--- app/views/shared/navbar.php
+++ app/views/shared/navbar.php
@@ -7,25 +7,33 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
+        <li class="nav-item <?php echo activeMenu('poomvc'); ?>">
           <a class="nav-link" href="<?php echo URLROOT; ?>">Home <span class="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item">
+        <?php if (isset($_SESSION['user_id'])) : ?>
+          <li class="nav-item <?php echo activeMenu('posts'); ?>">
+            <a class="nav-link" href="<?php echo URLROOT; ?>/posts">Posts</a>
+          </li>
+        <?php endif; ?>
+        <li class="nav-item <?php echo activeMenu('about'); ?>">
           <a class="nav-link" href="<?php echo URLROOT; ?>/paginas/about">About</a>
         </li>
       </ul>
-
+      
       <ul class="navbar-nav mr-auto">
         <?php if (isset($_SESSION['user_id'])) : ?>
-          <li class="nav-item">
+          <li class="nav-item ">
             <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Salir <span class="sr-only">(current)</span></a>
           </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="#"><?php echo $_SESSION['user_name']; ?></a>
+          </li>
         <?php else : ?>
-          <li class="nav-item">
+          <li class="nav-item <?php echo activeMenu('register'); ?>">
             <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">Registrar <span class="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
+          <li class="nav-item <?php echo activeMenu('login'); ?>">
+            <a class="nav-link " href="<?php echo URLROOT; ?>/users/login">Login</a>
           </li>
         <?php endif; ?>
       </ul>
